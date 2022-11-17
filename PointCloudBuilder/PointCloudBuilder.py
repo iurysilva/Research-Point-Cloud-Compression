268d0bb3ffd8993e0e4e8cadfec3617906c765ad
iurysilva <iuryglabson18@gmail.com>
2022-11-17 09:26:01 -0300

    Fix: Remove animation to improve performance
    Also add logs

--- PointCloudBuilder/PointCloudBuilder.py
+++ PointCloudBuilder/PointCloudBuilder.py
@@ -33,10 +33,14 @@ class PointCloudBuilder:
         self.ax1 = plt.axes(projection='3d')
 
     def build_one_frame(self, frame=False):
-        point_cloud = PyntCloud.from_file(self.filename_prefix + str(self.iteration) + '.ply')
+        point_cloud_name = self.filename_prefix + str(self.iteration) + '.ply'
+        print("Reading point cloud %d: %s..." %(self.jj, point_cloud_name))
+        point_cloud = PyntCloud.from_file(point_cloud_name)
+        print("Copying point cloud...")
         point_cloud2 = copy.deepcopy(point_cloud)
 
         # Cropping
+        print("Cropping...")
         location = np.array(point_cloud.points)[:, 0:3]
         indices = (location[:, 0] <= np.max(self.pstat[:, 0])) & (location[:, 0] >= np.min(self.pstat[:, 0])) & \
                   (location[:, 1] <= np.max(self.pstat[:, 1])) & (location[:, 1] >= np.min(self.pstat[:, 1])) & \
@@ -52,6 +56,7 @@ class PointCloudBuilder:
         indices = np.where(indices == 1)[0]
         point_cloud2.apply_filter(indices)
 
+        print("Centering Xs and Ys...")
         # unpacking point cloud x, y and y values
         location = np.array(point_cloud2.points)[:, 0:3]
         x = location[:, 0]
@@ -61,6 +66,7 @@ class PointCloudBuilder:
         inpt = np.round(stats.zscore(np.column_stack([x, y])), decimals=4)
 
         # creating the numerical model for getting the sensors and displacements
+        print("Creating polinomial model...")
         if self.jj == 0:
             self.inpt_hat = inpt[self.ss, :]
             fitted_parameters, p_cov = scipy.optimize.curve_fit(func, inpt, z)
@@ -69,10 +75,13 @@ class PointCloudBuilder:
             color = np.array(point_cloud2.points)[:, 3:6]
         else:
             fitted_parameters, p_cov = scipy.optimize.curve_fit(func, inpt, z)
+
+        print("Applying model...")    
         z_hat = func_array(self.inpt_hat, fitted_parameters[0], fitted_parameters[1], fitted_parameters[2],
                            fitted_parameters[3], fitted_parameters[4])
         aux = np.array([self.x_hat, self.y_hat, z_hat])
 
+        print("Creating and saving displacements...")
         if self.jj > 0:
             self.displacements[self.jj] = aux - self.z_hat_old
         else:
@@ -80,16 +89,19 @@ class PointCloudBuilder:
             self.displacements[self.jj] = np.zeros(aux.shape)
         self.z_hat_old = aux
 
+        print("Incrementing variables...")
         self.ff += 1
         self.jj += 1
         self.iteration += 1
 
+        print("Plotting scatter, saving figure...")
         self.scat3d.remove()
         self.scat3d = self.ax1.scatter3D(self.x_hat, self.y_hat, z_hat, c="gray")
         plt.savefig("plots/"+("img%d.jpg" % (self.iteration-40)))
         if self.iteration >= self.n_files:
+            np.save('arrays/displacements', self.displacements)
             self.anim.event_source.stop()
-        print("PC ", self.jj)
+        print("Point cloud %s finalized! \n" % self.jj)
 
     def run(self, animation_velocity=12000):
         self.scat3d = self.ax1.scatter3D(0, 0, 0)
@@ -99,5 +111,8 @@ class PointCloudBuilder:
         self.ax1.set_zlabel('Z (mm)')
         self.ax1.set_zlim3d(bottom=900, top=1250)
         print("Initializing with 3D animation")
-        self.anim = FuncAnimation(plt.gcf(), self.build_one_frame, interval=animation_velocity, repeat=False)
-        plt.show()
+        while True:
+            self.build_one_frame()
+
+        # self.anim = FuncAnimation(plt.gcf(), self.build_one_frame, interval=animation_velocity, repeat=False)
+        # plt.show()
